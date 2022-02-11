691cc9fe3a07c8923ec536ccc8c3f4c5fcebabd3
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2022-02-11 10:50:17 -0300

    NavBar style changed and ItemCount added

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,19 @@
+import ItemCount from "../ItemCount/ItemCount";
+import Container from "../../../node_modules/react-bootstrap/Container/";
+
 const ItemListContainer = () => {
-  return <div>Bienvenido</div>;
+  function handleOnAdd(countItem) {
+    if (countItem > 0) {
+      console.log(`Se agregaron ${countItem} productos`);
+    }
+  }
+
+  return (
+    <Container>
+      <div>Bienvenido</div>
+      <ItemCount stock={5} initial={0} onAdd={handleOnAdd}></ItemCount>
+    </Container>
+  );
 };
 
 export default ItemListContainer;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,30 +9,6 @@ import CartWidget from "../CartWidget/CartWidget";
 
 const NavBar = () => {
   return (
-    // <Navbar className="navbar px-5" variant="dark" expand="md">
-    //   <Container>
-    //     <Navbar.Brand href="#">Argentina 360</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="tienda-nav" />
-    //     <Navbar.Collapse id="tienda-nav">
-    //       <Nav
-    //         className="justify-content-end"
-    //         style={{ width: "100%", alignItems: "center" }} //por default pone los child items en stretch y puede dificultar UX
-    //       >
-    //         <Nav.Link href="#">Home</Nav.Link>
-    //         <Nav.Link href="#">Paquetes</Nav.Link>
-    //         <Nav.Link href="#">Excursiones</Nav.Link>
-    //         <Nav.Link href="#">Mapas</Nav.Link>
-    //         <Nav.Link href="#">Contacto</Nav.Link>
-    //         <Nav.Link href="#">
-    //           {" "}
-    //           <Button variant="light">Login</Button>
-    //         </Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //     <CartWidget />
-    //   </Container>
-    // </Navbar>
-
     <Navbar className="navbar px-5" variant="dark" expand={false}>
       <Container fluid>
         <Navbar.Toggle aria-controls="tiendaNavbar" />
