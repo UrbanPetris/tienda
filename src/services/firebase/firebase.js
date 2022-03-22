4e5106b5737d07b5ed3d7a7a67a8ea813b67ae8d
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2022-03-22 12:05:09 -0300

    agregado de formulario de contacto

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeTYzD4qgsSaS62BwMS14EmP6pg4EPLp4",
-  authDomain: "argentina360-b2b02.firebaseapp.com",
-  projectId: "argentina360-b2b02",
-  storageBucket: "argentina360-b2b02.appspot.com",
-  messagingSenderId: "221605920175",
-  appId: "1:221605920175:web:51603a39a5a938f18e435a",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
