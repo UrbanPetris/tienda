d3de27de06db4929c0c2be8e502389a02d3e3052
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2022-03-24 15:26:31 -0300

    updated backend function

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,26 +3,25 @@ import Container from "../../../node_modules/react-bootstrap/Container/";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import { doc, getDoc } from "firebase/firestore";
-import { firestoreDb } from "../../services/firebase/firebase";
+import { useNotificationServices } from "../../services/notification/NotificationServices";
+import { getProductById } from "../../services/firebase/firebase";
 
 const ItemDetailContainer = () => {
   const [product, setProduct] = useState([]);
   const { productId } = useParams();
   const [loading, setLoading] = useState(true);
+  const { setNotification, setMessageBackground } = useNotificationServices();
 
   useEffect(() => {
     setLoading(true); //cada vez que cambia de estado el productId se vuelve a ejecutar este setLoading(true) para simular el loading del componente
 
-    const docRef = doc(firestoreDb, "products", productId);
-
-    getDoc(docRef)
-      .then((doc) => {
-        const product = { id: doc.id, ...doc.data() };
-        setProduct(product);
+    getProductById(productId)
+      .then((response) => {
+        setProduct(response);
       })
       .catch((error) => {
-        console.log("Error getting document:", error);
+        setMessageBackground("danger");
+        setNotification("error", `Error buscando producto: ${error}`);
       })
       .finally(() => {
         setLoading(false);
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,32 +3,27 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemList from "../ItemList/ItemList";
 import Spinner from "../../../node_modules/react-bootstrap/Spinner/";
-import { getDocs, collection, query, where } from "firebase/firestore";
-import { firestoreDb } from "../../services/firebase/firebase";
+import { useNotificationServices } from "../../services/notification/NotificationServices";
+import { getProducts } from "../../services/firebase/firebase";
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([]); //tengo que inicializar el estado con un array si no el método de array de map en ItemList se rompe
   const { categoryId } = useParams();
   const [loading, setLoading] = useState(true);
+  const { setNotification, setMessageBackground } = useNotificationServices();
 
   useEffect(() => {
     //este products está dentro del scope de la callback de useEffects
     //el useEffect se ejecuta después de que se renderice el componente con el return
     setLoading(true); //cada vez que cambia de estado el categoryId se vuelve a ejecutar este setLoading(true) para simular el loading del componente
 
-    const collectionRef = categoryId
-      ? query(
-          collection(firestoreDb, "products"),
-          where("category", "==", categoryId)
-        )
-      : collection(firestoreDb, "products");
-
-    getDocs(collectionRef)
-      .then((querySnapshot) => {
-        const products = querySnapshot.docs.map((doc) => {
-          return { id: doc.id, ...doc.data() };
-        });
-        setProducts(products);
+    getProducts(categoryId)
+      .then((response) => {
+        setProducts(response);
+      })
+      .catch((error) => {
+        setMessageBackground("danger");
+        setNotification("error", error);
       })
       .finally(() => {
         setLoading(false);
