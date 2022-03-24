3fc0be4d888cd39dad6cb17c5f24e3e97781d751
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2022-03-24 15:42:59 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -5,41 +5,33 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Cart from "./components/Cart/Cart";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartContextProvider } from "./context/CartContext";
 import { NotificationServicesProvider } from "./services/notification/NotificationServices";
 import { ContactFormProvider } from "./context/ContactFormContext";
 
 function App() {
-  const location = useLocation();
-
   return (
     <div className="App">
       <NotificationServicesProvider>
         <ContactFormProvider>
           <CartContextProvider>
-            {/* <BrowserRouter> */}
-            <NavBar />
-            <TransitionGroup component={null}>
-              <CSSTransition key={location.key} classNames="fade" timeout={500}>
-                <Routes location={location}>
-                  {/* <Routes> */}
-                  <Route path="/" element={<ItemListContainer />} />
-                  <Route
-                    path="/category/:categoryId"
-                    element={<ItemListContainer />}
-                  />
-                  <Route
-                    path="/item/:productId"
-                    element={<ItemDetailContainer />}
-                  />
-                  <Route path="/Cart" element={<Cart />} />
-                  <Route path="/contactform" element={<ContactForm />} />
-                </Routes>
-              </CSSTransition>
-            </TransitionGroup>
-            {/* </BrowserRouter> */}
+            <BrowserRouter>
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<ItemListContainer />} />
+                <Route
+                  path="/category/:categoryId"
+                  element={<ItemListContainer />}
+                />
+                <Route
+                  path="/item/:productId"
+                  element={<ItemDetailContainer />}
+                />
+                <Route path="/Cart" element={<Cart />} />
+                <Route path="/contactform" element={<ContactForm />} />
+              </Routes>
+            </BrowserRouter>
           </CartContextProvider>
         </ContactFormProvider>
       </NotificationServicesProvider>
