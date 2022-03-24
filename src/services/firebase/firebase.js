5e1359f524b7ab17c4f08aaf82fa636887bac8b1
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2022-03-24 15:25:25 -0300

    backend functions added

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,5 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, Timestamp } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  query,
+  where,
+  getDoc,
+  doc,
+  addDoc,
+  documentId,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
@@ -13,3 +24,84 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const firestoreDb = getFirestore(app);
+
+export const getProducts = (categoryId) => {
+  return new Promise((resolve, reject) => {
+    const collectionRef = categoryId
+      ? query(
+          collection(firestoreDb, "products"),
+          where("category", "==", categoryId)
+        )
+      : collection(firestoreDb, "products");
+
+    getDocs(collectionRef)
+      .then((querySnapshot) => {
+        const products = querySnapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+
+        resolve(products);
+      })
+      .catch((error) => {
+        reject("Error obteniendo productos: ", error);
+      });
+  });
+};
+
+export const getProductById = (productId) => {
+  return new Promise((resolve, reject) => {
+    const docRef = doc(firestoreDb, "products", productId);
+
+    getDoc(docRef)
+      .then((response) => {
+        const product = { id: response.id, ...response.data() };
+        resolve(product);
+      })
+      .catch((error) => {
+        reject("Error obteniendo producto: ", error);
+      });
+  });
+};
+
+export const getProductsInOrder = (productIds) => {
+  return new Promise((resolve, reject) => {
+    const collectionRef = query(
+      collection(firestoreDb, "products"),
+      where(documentId(), "in", productIds)
+    );
+
+    getDocs(collectionRef)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject("Error obteniendo productos: ", error);
+      });
+  });
+};
+
+export const addOrder = (order) => {
+  return new Promise((resolve, reject) => {
+    addDoc(collection(firestoreDb, "orders"), order)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject("Error obteniendo producto: ", error);
+      });
+  });
+};
+
+export const batch = writeBatch(firestoreDb);
+
+export const getCategories = () => {
+  return new Promise((resolve, reject) => {
+    getDocs(collection(firestoreDb, "categories"))
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject("Error obteniendo categorías: ", error);
+      });
+  });
+};
